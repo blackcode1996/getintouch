76cfa18ed8fe355f6e56fec19c658d98b54ec7e1
Shashank Kumar <shashank9592sinha@gmail.com>
2023-04-29 19:20:49 +0530

    Initial commit

---
+++ backend/Get_In_Touch/src/main/java/com/getintouch/GetInTouchApplication.java
@@ -0,0 +1,13 @@
+package com.getintouch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetInTouchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetInTouchApplication.class, args);
+	}
+
+}
